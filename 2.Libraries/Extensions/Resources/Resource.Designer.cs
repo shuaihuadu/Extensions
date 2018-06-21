468e6e0365cb88371af023c740073febb5c4fc49
ShuaiHua Du <shuaihua.du@outlook.com>
2018-06-21 11:01:38 +0800

    Add Boolean extensions.

--- 2.Libraries/Extensions/Resources/Resource.Designer.cs
+++ 2.Libraries/Extensions/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace Extensions.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resource {
@@ -142,6 +142,15 @@ namespace Extensions.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to 否.
+        /// </summary>
+        internal static string No {
+            get {
+                return ResourceManager.GetString("No", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to 秒前.
         /// </summary>
         internal static string SecondAgo {
@@ -192,6 +201,15 @@ namespace Extensions.Resources {
         internal static string YearsAgo {
             get {
                 return ResourceManager.GetString("YearsAgo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 是.
+        /// </summary>
+        internal static string Yes {
+            get {
+                return ResourceManager.GetString("Yes", resourceCulture);
             }
         }
     }
---
+++ 2.Libraries/Extensions/System/BooleanExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Extensions.Resources;
+
+namespace System
+{
+    /// <summary>
+    /// The <see cref="bool"/> extensions.
+    /// </summary>
+    public static class BooleanExtensions
+    {
+        /// <summary>
+        /// To the yes or no string.
+        /// </summary>
+        /// <param name="value">if set to <c>true</c> return <paramref name="yesString"/> ;otherwise <paramref name="noString"/> .</param>
+        /// <param name="yesString">The yes string.</param>
+        /// <param name="noString">The no string.</param>
+        /// <returns><paramref name="yesString"/> or <paramref name="noString"/></returns>
+        public static string ToYesOrNoString(this bool value, string yesString = "", string noString = "")
+        {
+            if (yesString.IsNullOrBlank())
+            {
+                yesString = Resource.Yes;
+            }
+            if (noString.IsNullOrBlank())
+            {
+                noString = Resource.No;
+            }
+            return value ? yesString : noString;
+        }
+    }
+}
