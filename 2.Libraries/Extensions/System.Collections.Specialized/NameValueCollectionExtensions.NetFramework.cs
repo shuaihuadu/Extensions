7c5f742b5474e0b334b05b424829f8255d811a6b
ShuaiHua Du <shuaihua.du@outlook.com>
2019-02-26 17:05:46 +0800

    Add .net core build.

---
+++ 2.Libraries/Extensions/System.Collections.Specialized/NameValueCollectionExtensions.NetFramework.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Routing;
+namespace System.Collections.Specialized
+{
+    /// <summary>
+    /// Common extensions of <see cref="NameValueCollection"/>
+    /// </summary>
+    public static partial class NameValueCollectionExtensions
+    {
+        /// <summary>
+        /// Convert the <see cref="NameValueCollection"/> to <see cref="RouteValueDictionary"/>.
+        /// </summary>
+        /// <param name="collection">The <see cref="NameValueCollection"/></param>
+        /// <returns>The <see cref="RouteValueDictionary"/> converted.</returns>
+        public static RouteValueDictionary ToRouteValues(this NameValueCollection collection)
+        {
+            if (collection.IsNull() || collection.HasKeys() == false)
+            {
+                return new RouteValueDictionary();
+            }
+            var routeValues = new RouteValueDictionary();
+            foreach (string key in collection.AllKeys)
+            {
+                routeValues.Add(key, collection[key]);
+            }
+            return routeValues;
+        }
+    }
+}
--- 2.Libraries/Extensions/System.Collections.Specialized/NameValueCollectionExtensions.cs
+++ 2.Libraries/Extensions/System.Collections.Specialized/NameValueCollectionExtensions.cs
@@ -1,12 +1,10 @@
 ﻿using System.Linq;
-using System.Web.Routing;
-
 namespace System.Collections.Specialized
 {
     /// <summary>
     /// Common extensions of <see cref="NameValueCollection"/>
     /// </summary>
-    public static class NameValueCollectionExtensions
+    public static partial class NameValueCollectionExtensions
     {
         /// <summary>
         /// Gets the value of <typeparamref name="T"/> associated with the specified key from the <see cref="NameValueCollection"/>.
@@ -28,24 +26,6 @@ namespace System.Collections.Specialized
                 return defaultValue;
             }
             return returnValue;
-        }
-        /// <summary>
-        /// Convert the <see cref="NameValueCollection"/> to <see cref="RouteValueDictionary"/>.
-        /// </summary>
-        /// <param name="collection">The <see cref="NameValueCollection"/></param>
-        /// <returns>The <see cref="RouteValueDictionary"/> converted.</returns>
-        public static RouteValueDictionary ToRouteValues(this NameValueCollection collection)
-        {
-            if (collection.IsNull() || collection.HasKeys() == false)
-            {
-                return new RouteValueDictionary();
-            }
-            var routeValues = new RouteValueDictionary();
-            foreach (string key in collection.AllKeys)
-            {
-                routeValues.Add(key, collection[key]);
-            }
-            return routeValues;
         }
     }
 }
